37c79d0dfcbba8fef2849507e25cadf7f3c7701d
chrissx@ThinkBadly <chrissx@chrissx.de>
2020-03-12 21:20:37 +0100

    pons and pons_test: refactoring and code simplifications

--- pons.py
+++ pons.py
@@ -1,23 +1,38 @@
 import re
 from urllib.request import *
-from json import JSONDecoder, JSONEncoder
+from json import JSONDecoder
 
-def get_pons_dicts(lang):
+def dicts(lang):
     langs = []
     for lang in JSONDecoder().decode(urlopen('https://api.pons.com/v1/dictionaries?language={}'.format(lang)).read().decode('utf-8')):
         lang.pop('directed_label', None)
         langs.append(lang)
     return langs
 
-def get_pons_translations(pons_dict, src_word, pons_secret):
+_subs = [
+            ['<.+?>', ''],
+            ['\\\\u200a', ' '],
+            ['  ', ' '],
+        ]
+
+def raw_translations(pons_dict, src_word, pons_secret):
     req = Request('https://api.pons.com/v1/dictionary?q={}&l={}'.format(src_word, pons_dict))
     req.add_header('X-Secret', pons_secret)
-    content = urlopen(req).read().decode('utf-8')
-    content = re.sub('<.+?>', '', content)
-    content = JSONDecoder().decode(content)[0]['hits'][0]['roms'][0]['arabs']
+    t = urlopen(req).read().decode('utf-8')
+    for sub in _subs:
+        t = re.sub(sub[0], sub[1], t)
+    return t
+
+def _parse_content(translations, content):
+    for t in content:
+        if 'type' in t and t['type'] == 'entry':
+            _parse_content(translations, t['roms'][0]['arabs'][0]['translations'])
+        if 'target' in t and len(t['target']) > 0:
+            translations.append(t)
+
+def translations(pons_dict, src_word, pons_secret):
+    content = raw_translations(pons_dict, src_word, pons_secret)
+    content = JSONDecoder().decode(content)[0]['hits']
     translations = []
-    for a in content:
-        for t in a['translations']:
-            if len(t['target']) > 0:
-                translations.append(t)
+    _parse_content(translations, content)
     return translations
--- pons_test.py
+++ pons_test.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python3
 
-from pons import *
+import pons
 from sys import argv, exit
 from pprint import pprint
+from json import JSONDecoder
 
-if(len(argv) < 2):
+if len(argv) < 2:
     print("Usage: ./pons_test.py [pons secret]")
     exit(-1)
 
-pprint(get_pons_dicts('de'))
-pprint(get_pons_translations('dela', 'arcere', argv[1]))
+def __test(title, result):
+    print('\n\n--{}--\n\n'.format(title))
+    pprint(result)
+
+def dicts(lang):
+    __test('DICTS ({})'.format(lang), pons.dicts(lang))
+
+def translations(pons_dict, word):
+    __test(word, pons.translations(pons_dict, word, argv[1]))
+
+def raw(pons_dict, word, decoded_and_pretty):
+    raw = pons.raw_translations(pons_dict, word, argv[1])
+    if decoded_and_pretty:
+        raw = JSONDecoder().decode(raw)
+    __test('{} (RAW)'.format(word), raw)
+
+def run_tests():
+    dicts('de')
+    translations('dela', 'arcere')
+    translations('dela', 'nonnumquam')
+    translations('dela', 'vir')
+    raw('dela', 'nonnumquam', True)
+
+def main():
+    run_tests()
+
+if __name__ == '__main__':
+    main()
